b8b1bc976ccc59664f7d551da82c9b219c280ffe
DawsonSilkenat <Dawsonsilkenat@gmail.com>
2022-02-14 13:56:04 +0000

    feel like there are better ways of doing this

--- code/data_parsing_example.py
+++ code/data_parsing_example.py
@@ -1,36 +1,99 @@
-
-
-from re import U
+from typing import Sequence
 from simplediff import diff
+import re 
 
-if __name__ == "__main__":
-    # Probably should fix the file system a bit
-    data = open("bias_data/bias_data/WNC/biased.word.dev", "r")
-    # Each line is a single datapoint
-    line = data.readline()
-    # The relevent information is split by tab characters
-    line = line.split("\t")
+# import nltk
+# nltk.download("stopwords")
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer # Consider using this to trim words, just not sure how it will work with stopwords
+import torch
 
-    # These are the parts we care about, the before and after change
-    original = line[3]
-    updated = line[4]
-    print(original)
-    print(updated)
-    print()
 
-    # List of words rather than single string
-    original = original.split(" ")
-    updated = updated.split(" ")
-    print(original)
-    print(updated)
-    print()
 
-    # Comparison between the lists
-    diffs = diff(original, updated)
-    print(diffs)
-    print()
+class Language_dataset(torch.utils.data.IterableDataset):
+    def __init__(self, file_name="bias_data/bias_data/WNC/biased.word.dev", stem=False, remove_stopwords=False):
+        self.data = open(file_name, "r") 
+        self.file_name = file_name 
+        self.stem = stem
+        self.remove_stopwords = remove_stopwords
 
-    # The word that was changed
-    diffs = list(filter((lambda x: x[0] != "="), diffs))
-    print(diffs)
-    print()
+    def reset(self):
+        self.data = open(self.file_name, "r")   
+
+    def get_sample(self):
+        for line in self.data:
+            # # Each line is a single datapoint; The relevent information is split by tab characters
+            # line = self.data.readline()
+            line = line.split("\t")
+
+            # These are the parts we care about, the before and after change
+            original = line[3]
+            updated = line[4]
+
+            original = original.lower()
+            updated = updated.lower()
+
+            # Where do I apply this? Needs 
+            # if self.stem:
+            #     ps = PorterStemmer()
+            # else:
+            #     ps = lambda x: x
+
+            # Remove non-letter characters. Note the space in the regex is deliberate 
+            original = re.sub("[^A-Za-z ']+", "", original) 
+            updated = re.sub("[^A-Za-z ']+", "", updated) 
+
+            # List of words rather than single string
+            original = original.split()
+            updated = updated.split()
+            
+            # Simplify the data by removing the words that provide little information
+            if self.remove_stopwords: 
+                stop_words = stopwords.words("english")
+                original = [word for word in original if word not in stop_words and word != ""]
+                updated = [word for word in updated if word not in stop_words and word != ""]
+
+            print(original)
+            print(updated)
+
+            # Comparison between the lists
+            diffs = diff(original, updated)
+
+            print(diffs)
+
+            # Find the index of every removed word
+            diffs = list(filter((lambda x: x[0] != "+"), diffs))
+            indeces = []
+            index = 0
+            for sequence in diffs:
+                if sequence[0] == "=":
+                    index += len(sequence[1]) 
+                elif sequence[0] == "-":
+                    for _ in range(len(sequence[1])):
+                        indeces.append(index)
+                        index += 1
+                else:
+                    print("Unexpected behavour when determining indices") 
+                
+            print(line[3])
+            print()
+            print(line[4])
+            print()
+            for i in indeces:
+                print(original[i])
+                print()
+            yield (original, indeces)
+
+        # We have iterated over all elements of the file, to go over them again a call to reset should be required
+        self.data.close()
+
+        # # We specifically care about the words that were removed 
+        # diffs = list(filter((lambda x: x[0] == "-"), diffs))
+        # diffs = [word for diff in diffs for word in diff[1:]]
+
+    def __iter__(self):
+        return self.get_sample()
+
+
+        
+
